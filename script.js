640f729cb9022c0ae4d0a15865bea7477a2fad21
mblekacic <mblekacic@playnirvana.com>
2024-04-10 18:34:03 +0200

    new project

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName('pravokutnik')[0].addEventListener('click', function() {
+    this.style.backgroundColor = this.style.backgroundColor === 'blue' ? 'red' : 'blue';
+});
