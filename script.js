1536d295f0e3a5b415e78f783c4053e5d0e93d56
Mblekacic <marijetabl@gmail.com>
2024-04-10 19:11:48 +0200

    promjena

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementsByClassName('pravokutnik')[0].addEventListener('click', function() {
     this.style.backgroundColor = this.style.backgroundColor === 'blue' ? 'red' : 'blue';
 });
+
+document
